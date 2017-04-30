6b5e98acc01d054fbc200c519d30f9b6f74b15ee
jweihsz <1989428128@qq.com>
2017-04-30 20:17:48 +0800

    add the list

---
+++ reactCar/components/list.js
@@ -0,0 +1,27 @@
+import  React,{Component} from  "react";
+import Item from "./item";
+import _ from  "underscore";
+
+
+export default class  List   extends  Component{
+
+
+	renderItems(){
+
+		const props = _.omit(this.props,"items");
+		return(_.map(this.props.items,(item,index)=><Item  key={index} {...item} {...props}  />));
+	}
+	render(){
+
+		return(
+
+			<tbody>
+				{this.renderItems()}
+			</tbody>
+		);
+	}
+}
+
+
+
+
